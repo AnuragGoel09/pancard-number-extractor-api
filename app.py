37ccc37f648553d7b8ca7533a33defa28e670743
AnuragGoel09 <goelanurag2003@gmail.com>
2024-05-11 21:33:47 +0530

    pan number extractor

--- app.py
+++ app.py
@@ -3,9 +3,11 @@
 from flask import Flask, request, jsonify 
 from PIL import Image
 import easyocr
-import io
+import cv2
+import numpy as np
 reader = easyocr.Reader(['en'])
 app = Flask(__name__)
+
 
 @app.route('/upload',methods=['GET'])
 def extract_text():
@@ -13,20 +15,92 @@ def extract_text():
         return jsonify({'error': 'No image uploaded'}), 400
 
     image_file = request.files['image']
+
     if image_file.filename == '':
         return jsonify({'error': 'No selected image'}), 400
 
     try:
-        # Read image from the file upload
-        # image = Image.open(io.BytesIO(image_file.read()))
         image=image_file.read()
-        # Perform OCR using pytesseract
-        extracted_text = reader.readtext(image)
-        text = [entry[1] for entry in extracted_text]
-        return jsonify({'text': text}), 200
+        nparr = np.frombuffer(image, np.uint8)
+        image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        pan_number=get_pan_number(image)
+        return jsonify({'text': pan_number}), 200
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
+def get_pan_number(image):
+    extracted_text = reader.readtext(image)
+    result= [entry[1] for entry in extracted_text]
+    
+    for i in result:
+        if len(i)==0:
+            result.remove(i)
+    for item in result:
+        if len(item)==10:
+            check=True
+            for i in item:
+                if (i>='A' and i<='Z') or (i>='0' and i<='9'):
+                    a=1
+                else:
+                    check=False
+                    break
+            if check:
+                flag=check_pan(item)
+                if flag:
+                    return item
+    idx=0
+    for item in result:
+        if "Number" in item:
+            flag=check_pan(item)
+            if flag:
+                return [result[idx+1][:10]]
+        idx+=1
+    img=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+    ans=[]
+    for i in range(70,150,10):
+        a=i
+        for j in range(150,70,-10):
+            t,new_img=cv2.threshold(img,a,j,cv2.THRESH_BINARY)
+            extracted_text = reader.readtext(img)
+            result = [entry[1] for entry in extracted_text]            
+            for i in result:
+                if len(i)==0:
+                    result.remove(i)
+            for item in result:
+                if len(item)==10:
+                    check=True
+                    for i in item:
+                        if (i>='A' and i<='Z') or (i>='0' and i<='9'):
+                            a=1
+                        else:
+                            check=False
+                            break
+                    if check:
+                        flag=check_pan(item)
+                        if flag:
+                            return item    
+    return "image not proper"
+
+def check_pan(name):
+    if len(name)!=10:
+        return False
+    check=True
+    for i in range(5):
+        if name[i]>='A' and name[i]<='Z':
+            check=True
+        else:
+            return False
+    for i in range(5,9):
+        if name[i]>='0' and name[i]<='9':
+            check=True
+        else:
+            return False
+    if name[-1]>='A' and name[-1]<='Z':
+        check=True
+    else:
+        return False
+    return True
+
+
 if __name__ == '__main__':
-    # app.run(debug=False, host='0.0.0.0')
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
