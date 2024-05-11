f66f0b698b02fa57336eb07653d3c324e53c21f5
AnuragGoel09 <goelanurag2003@gmail.com>
2024-05-11 19:16:17 +0530

    testing

---
+++ app.py
@@ -0,0 +1,32 @@
+# app.py
+
+from flask import Flask, request, jsonify 
+from PIL import Image
+import pytesseract
+import io
+pytesseract.pytesseract.tesseract_cmd = "/app/.apt/usr/bin/tesseract"
+
+app = Flask(__name__)
+
+@app.route('/upload',methods=['GET'])
+def extract_text():
+    if 'image' not in request.files:
+        return jsonify({'error': 'No image uploaded'}), 400
+
+    image_file = request.files['image']
+    if image_file.filename == '':
+        return jsonify({'error': 'No selected image'}), 400
+
+    try:
+        # Read image from the file upload
+        image = Image.open(io.BytesIO(image_file.read()))
+
+        # Perform OCR using pytesseract
+        extracted_text = pytesseract.image_to_string(image)
+
+        return jsonify({'text': extracted_text}), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True)
